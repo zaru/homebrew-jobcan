211ca49d11b80cac6d7b008b2bd4de6e30e19e9f
zaru <zarutofu@gmail.com>
2018-02-08 00:34:30 +0900

    jobcan version v0.2.3

--- jobcan.rb
+++ jobcan.rb
@@ -1,9 +1,9 @@
 class Jobcan < Formula
   desc "jobcan cli"
   homepage "https://github.com/zaru/jobcan"
-  url "https://github.com/zaru/jobcan/releases/download/v0.2.2/jobcan_0.2.2_darwin_amd64.tar.gz"
-  version "0.2.2"
-  sha256 "f54df6a134ef5b3e5b036a56ad9de8585dfcaf9be524259afe6b35f09ad69389"
+  url "https://github.com/zaru/jobcan/releases/download/v0.2.3/jobcan_0.2.3_darwin_amd64.tar.gz"
+  version "0.2.3"
+  sha256 "fc416cb424b9f32d0fb091184fc0756cb820ed0067d3cd241d538881a5f3d132"
   
   depends_on "fzf"
 
